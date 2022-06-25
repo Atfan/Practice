9e273c95425775fe605b785da34b6844fbf500b7
Lifes <atfan235@gmail.com>
2022-06-25 11:01:41 +0300

    Update student

--- Student.cpp
+++ Student.cpp
@@ -1,1 +1,17 @@
 #include "Student.h"
+
+
+Student::Student()
+{
+	date d(1, 1, 1900);
+	this->SurName = "Unknown";
+	this->FirstName = "Unknown";
+	this->SecondName = "Unknown";
+	this->birthday = d;
+	this->phone = "Unknown";
+
+}
+
+Student::~Student()
+{
+}
--- Student.h
+++ Student.h
@@ -1,1 +1,38 @@
 #pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct date
+{
+	int day;
+	int month;
+	int year;
+	
+	date(int day,int month, int year)
+	{
+		this->day = day;
+		this->month = month;
+		this->year = year;
+	}
+};
+
+class Student
+{
+public:
+	Student();
+	~Student();
+
+private:
+	string SurName;
+	string FirstName;
+	string SecondName;
+	date   birthday;
+	string phone;
+	string institution;
+	string address;
+	string groupe;
+
+
+	
+};
